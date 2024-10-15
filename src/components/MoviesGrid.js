d13d710ce09caae877b0a2c0f09af9f71ff8fa10
MELAMED, LUCILA (Servicio Externo en YPF) <SE4412@grupo.ypf.com>
2024-10-15 13:07:15 -0300

    props y filter en real time

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,29 +1,39 @@
-import React, {useState, useEffect} from 'react';
-import '../styles.css'
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data));
-    }, []);
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
 
-    return (
-        <div className='movies-grid'>
-            {
-                movies.map(movie => (
-                    <div key={movie.id} className='movie-card'>
-                        <img src={`images/${movie.image}`} alt={movie.title}/>
-                        <div className='movie-card-info'>
-                            <h3 className='movie-card-title'> {movie.title} </h3>
-                            <p className='movie-card-genre'> {movie.genre} </p>
-                            <p className='movie-card-rating'> {movie.rating} </p>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
-    );
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
