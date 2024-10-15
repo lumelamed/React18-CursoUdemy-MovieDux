6116916614eec63887e2c1b3bea06f4cd51f040c
MELAMED, LUCILA (Servicio Externo en YPF) <SE4412@grupo.ypf.com>
2024-10-15 15:55:14 -0300

    rutas distintas para grilla y watchlist con Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid movies={movies} />}></Route>
+            <Route path="/watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer></Footer>
     </div>
